f9c4021d0ac9ffd267994fc4b9fccc29460b42ab
Narayan1180 <narayanchandra952@gmail.com>
2024-01-07 23:18:18 +0530

    test

--- mrsncv/settings.py
+++ mrsncv/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -79,7 +81,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES = {
 
+    'default':dj_database_url.parse(os.environ.get("DATABASE_URL") )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
